0be94b47e2712bd1adc5d103319abc9bfc96b5c1
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2022-06-03 23:44:21 +0600

    condition

---
+++ M4/m18/5_condition.js
@@ -0,0 +1,17 @@
+// Condition in JS
+
+/*
+syntax: 
+if (condition) {
+    code to execute
+}
+*/
+
+var age = 22;
+
+if (age ==22){
+    console.log("Yes, Age is 22");
+}
+else{
+    console.log("No, age is not 22");
+}
